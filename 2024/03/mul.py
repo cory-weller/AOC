907dae29def4e81329a24adbf69723e10c11de71
Cory <cory.a.weller@gmail.com>
2024-12-06 17:18:31 +0000

    add part two

--- 2024/03/mul.py
+++ 2024/03/mul.py
@@ -9,10 +9,28 @@ matches = re.findall(pattern='mul\\([0-9]+,[0-9]+\\)', string=text)
 
 count = 0
 for i in matches:
-    print(i)
     vals = re.split('\\(|,|\\)', i)[1:3]
     product = int(vals[0]) * int(vals[1])
-    print(product)
     count += product
+
+print(count)
+
+# Part two
+
+# prefix with ON and end with OFF
+text = 'do()' + text + "don't()"
+
+# get good sections
+good_parts = re.findall(pattern="do\\(\\).*?don't\\(\\)", string=text)
+
+
+count = 0
+
+for i in good_parts:
+    matches = re.findall(pattern='mul\\([0-9]+,[0-9]+\\)', string=i)
+    for match in matches:
+        vals = re.split('\\(|,|\\)', match)[1:3]
+        product = int(vals[0]) * int(vals[1])
+        count += product
 
 print(count)
