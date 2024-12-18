31e855d94a0a0c3fb6793e23490466b6bf959cbc
Cory <cory.a.weller@gmail.com>
2024-12-18 14:44:37 +0000

    ez pz

--- 2024/11/stones.py
+++ 2024/11/stones.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-# infilename = 'tiny.txt'
 infilename = 'input.txt'
 
 with open(infilename, 'r', encoding='UTF-8') as infile:
     start = infile.read().strip().split()
     start = [int(x) for x in start]
+
 
 def mutate(x):
     if x==0:
@@ -19,52 +19,35 @@ def mutate(x):
     else:
         return([2024*x])
 
-def flatten(nested_list):
-    return [x for lists in nested_list for x in lists]
 
 blinks = 75
 
-for i in range(blinks):
-    #print(start)
-    start = flatten([mutate(x) for x in start])
-    print(len(set(start)))
 
-while True:
-    i = max(start)
-    print(i)
-    start = mutate(i)
-
-
-# Part 2
-
-
-count = 0
-def recursive_mutate(N, blinks, max_blinks):
-    global count
-    if blinks >= max_blinks:
-        count += 1
+# Initialize stones dict
+stones = {}
+for i in start:
+    if i not in stones:
+        stones[i] = 1
     else:
-        for i in mutate(N):
-            recursive_mutate(i, blinks+1, max_blinks)
+        stones[i] += 1
 
 
-for i in start[1:2]:
-    recursive_mutate(N=i, blinks=0, max_blinks=40)
+for n in range(blinks):
+    next_layer = {}
+    for i in stones.keys():
+        count = stones[i]
+        new_stones = mutate(i)
+        for j in new_stones:
+            try: next_layer[j] += count
+            except KeyError: next_layer[j] = count
+    stones = next_layer
 
-print(count)
+total = 0
+
+for i in stones:
+    total += stones[i]
+
+print(total)
 
 
-branches = {}
-
-for i in range(10):
-    for mb in range(50):
-        count = 0
-        recursive_mutate(N=i, blinks=0, max_blinks=mb)
-
-
-
-
-print(count)
-
-
-    
+exit()
